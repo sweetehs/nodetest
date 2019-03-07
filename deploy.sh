59bc0c95472ee32bb071ce22119e7ee5a7807851
zhaoweinan.vernon <zhaoweinan.vernon@byte.dance.com>
2019-03-07 19:04:44 +0800

    fix bug

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,12 @@ FRONT="$ROOT/node-test-f"
 SERVER="$ROOT/node-test"
 
 cd $SERVER
+mkdir $SERVER/src/git
 npm install
 rm -rf $SERVER/src/dist
 
 cd $FRONT
+npm install
 npm run build
 mv -f $FRONT/dist $SERVER/src
 
